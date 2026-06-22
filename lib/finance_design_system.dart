fe82ef99ca85f967a366ded5069908d2ded2b3b5
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-06-22 17:13:03 -0400

    Initial commit

---
+++ lib/finance_design_system.dart
@@ -0,0 +1,9 @@
+export 'src/theme/app_colors.dart';
+export 'src/theme/app_text_styles.dart';
+export 'src/theme/app_theme.dart';
+
+export 'src/widgets/app_button.dart';
+export 'src/widgets/app_card.dart';
+export 'src/widgets/app_text_field.dart';
+
+export 'src/tokens/app_spacing.dart';
